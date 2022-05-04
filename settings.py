f4ec02b7bee5c046881c7d9404e3fa5d52d37ddd
SonSahip <86627680+SonSahip@users.noreply.github.com>
2022-05-04 21:36:31 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "123456789876:Abkkanbsbaks"
+TOKEN = "5308810092:AAEvTWQY99EvjaoJMpYw7Sbvhy5BEUF80GU"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
